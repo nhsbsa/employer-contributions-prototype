3d6210bf713d9befda53a14cd102b07a9f35482f
Cesare <cesarecontini@gmail.com>
2017-01-27 16:57:11 +0000

    Amended deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,6 @@ cd .deploy
 git add -A
 git commit -m "Deploying and releasing version $PACKAGE_VERSION"
 git tag $PACKAGE_VERSION
-git push && git push --tags
+git push origin master && git push --tags
 cd ..
 rm -rf .deploy
