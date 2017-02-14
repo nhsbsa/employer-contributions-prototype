4bd4c901313bec2709a76f306f6ee367fe9d0cc0
Cesare Giani Contini <cesare.gianicontini@nhs.net>
2017-02-14 12:27:36 +0000

    Amended deploy script, added banner and commit and push script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,29 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
-git config --global user.email "gitlabrunner@nhs.com"
-git config --global user.name "Gitlabrunner"
+DESCRIPTION=$(cat package.json | grep description | sed 's/\"description\"//gmi' | tr \" ' ' | sed 's/\:  //g')
+
+GIT_CURRENT_EMAIL=$(git config --global --get user.email)
+GIT_CURRENT_USERNAME=$(git config --global --get user.name)
+
+if [ -v $GIT_CURRENT_EMAIL ]
+    then
+        echo "GIT USER EMAIL SET AS: $GIT_CURRENT_EMAIL"
+        echo "GIT USER NAME SET AS: $GIT_CURRENT_USERNAME"
+        git config --global user.email "$GIT_CURRENT_EMAIL"
+        git config --global user.name "$GIT_CURRENT_USERNAME"
+elif [ -v $GITLABRUNNER_EMAIL ]
+    then
+        echo "GIT USER EMAIL SET AS: $GITLABRUNNER_EMAIL"
+        echo "GIT USER NAME SET AS: $GITLABRUNNER_NAME"
+        git config --global user.email "$GITLABRUNNER_EMAIL"
+        git config --global user.name "$GITLABRUNNER_NAME"
+    else
+        git config --global user.email gitlabrunner@nhs.net
+        git config --global user.name gitlabrunner
+fi
+
+
 git clone git@gitlab00.node.consul:bsa-design/nhs-template.git .deploy
 # git clone ssh://git@localhost:10022/home-styles/nhs-template.git .deploy
 # git clone git@gitlab.domain.net:group-name/project-name.git .deploy
@@ -14,6 +35,7 @@ rm -rf .deploy/images .deploy/javascripts .deploy/stylesheets
 cp -R dist/* .deploy
 cd .deploy
 # sed -i "" "s/\"version\": \".*\",/\"version\": \"$PACKAGE_VERSION\",/" package.json
+echo -e "$(date) - VERSION $PACKAGE_VERSION\n $DESCRIPTION\n\n" >> version-log.txt
 git add -A
 git commit -m "Deploying and releasing version $PACKAGE_VERSION"
 git tag $PACKAGE_VERSION
