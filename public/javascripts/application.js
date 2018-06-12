0584fea14176422f7f9f265ecb8116a970d67376
chmoo@NHSBSA.NHS.UK <christopher.moore6@nhs.net>
2018-06-12 12:49:43 +0100

    front-end accessibility amends

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
 
   // Use GOV.UK selection-buttons.js to set selected
   // and focused states for block labels
-  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
-  new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
+  // var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
+  // new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
 
   // Where .block-label uses the data-target attribute
   // to toggle hidden content
